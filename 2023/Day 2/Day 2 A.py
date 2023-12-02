34ede63026e37c784cae91633629a5ddfde27e8f
Neel Patel <neelp@hotmail.co.uk>
2023-12-02 00:20:12 +0000

    Uploading premature Day 2

---
+++ 2023/Day 2/Day 2 A.py
@@ -0,0 +1,7 @@
+with open(r'D:\Documents\Programming\Python Projects\Advent of Code\2023\Day 1\inputs.txt', 'r') as file:
+
+    # Read through all file lines.
+    lines = file.readlines()
+
+    # Iterate through each line in text file.
+    for line in lines:
