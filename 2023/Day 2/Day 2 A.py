1aed88d04fdcec8ce396f9cfd3cc8b6d636f572b
Neel Patel <neelp@hotmail.co.uk>
2024-09-15 02:27:24 +0100

    Updating Day 2 and 3 Code

--- 2023/Day 2/Day 2 A.py
+++ 2023/Day 2/Day 2 A.py
@@ -1,7 +1,18 @@
-with open(r'D:\Documents\Programming\Python Projects\Advent of Code\2023\Day 1\inputs.txt', 'r') as file:
+limit_dict = {'red': 12, 'green': 13, 'blue': 14}
+game_sum = 0
+
+with open(r'D:\Documents\Programming\Python Projects\Advent of Code\2023\Day 2\inputs.txt', 'r') as file:
 
     # Read through all file lines.
     lines = file.readlines()
 
     # Iterate through each line in text file.
     for line in lines:
+
+        line = line.strip('\n').replace(' ', '')
+        split_lines = line.split(':')
+        game_num = int(split_lines[0].strip('Game'))
+        games = split_lines[1].split(';')
+
+        for game in games:
+            
